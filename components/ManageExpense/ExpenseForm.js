1d05660120284cead1d95c8e4b0454fd29e27cc3
Parth Wani <parthwani22@gmail.com>
2025-04-20 14:58:20 -0500

    Added styling to input components

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 
 function ExpenseForm() {
@@ -7,15 +7,18 @@ function ExpenseForm() {
 
     return (
         <View>
-            <Input label='Amount' textInputConfig={{
-                keyboardType: 'decimal-pad',
-                onChangeText: amountChangedHandler,
-            }}/>
-            <Input label='Date' textInputConfig={{
-                placeholder: 'YYYY-MM-DD',
-                maxLength: 10,
-                onChangeText: () => {}
-            }}/>
+            <Text style={styles.titleStyle}>Your Expense</Text>
+            <View style={styles.inputsRow}>
+                <Input label='Amount' style={styles.rowInput} textInputConfig={{
+                    keyboardType: 'decimal-pad',
+                    onChangeText: amountChangedHandler,
+                }}/>
+                <Input label='Date' style={styles.rowInput} textInputConfig={{
+                    placeholder: 'YYYY-MM-DD',
+                    maxLength: 10,
+                    onChangeText: () => {}
+                }}/>
+            </View>
             <Input label='Description'textInputConfig={{
                 multiline: true,
                 numberOfLines: 10
@@ -26,4 +29,24 @@ function ExpenseForm() {
     );
 };
 
-export default ExpenseForm
+export default ExpenseForm;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        marginTop: 40,
+    },
+    titleStyle: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        color: 'white',
+        marginVertical: 24,
+        textAlign: 'center'
+    },
+    inputsRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    rowInput: {
+        flex: 1
+    }
+})
